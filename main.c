e6ae7891353c8e68e6fb05452e7d9e7d5a28f80c
saleh <saleh@localhost>
2021-01-31 00:00:26 +0100

    Gestion erreur

--- main.c
+++ main.c
@@ -10,8 +10,9 @@
 
 #define FICHIER_CLE "cle.serv"
 
-void arreter_processus(int signal);
-int set_signal_handler(int sig, void (*handler)(int));
+void arreter_processus(int signal);//Arrete tout les processus à la réception du signal
+int set_signal_handler(int sig, void (*handler)(int));//Set le signal
+void erreur(char * texte_erreur);//Gère les erreurs
 
 //argument nb_serveurs nb_cuisiniers nb_term nb_spec nb_1 nb_2 ... nb_k
 int main(int argc, char const *argv[])
@@ -21,17 +22,21 @@ int main(int argc, char const *argv[])
 
     cle = ftok(FICHIER_CLE,'a');
 
+    if (cle == -1)
+    {
+        erreur("Erreur lors de la création de la clé...");
+    }
+    
+
    if (set_signal_handler(SIGINT,arreter_processus) != 0)
    {
-       perror("Erreur signal (set)...");
-       exit(-1);
+       erreur("Erreur signal (set)...");
    }
    
 
     if (argc <= 5)
     {
-        printf("Usage : <nb_serveurs> <nb_cuisiniers> <nb_term> <nb_spec> <nb_1> <nb_2> ... <nb_k>\n");
-        exit(-1);
+       erreur("Usage : <nb_serveurs> <nb_cuisiniers> <nb_term> <nb_spec> <nb_1> <nb_2> ... <nb_k>");
     }
    
     
@@ -55,6 +60,11 @@ int set_signal_handler(int sig, void (*handler)(int))
     return sigaction(sig,&sgnl,NULL);
 }
 
+void erreur(char * texte_erreur)
+{
+    printf("%s\n",texte_erreur);
+    exit(-1);
+}
 
 
 
